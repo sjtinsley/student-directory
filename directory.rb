75bf16402da4aea648d978999b50789757ad0f02
sjtinsley <simonjtinsley@gmail.com>
2022-04-13 15:26:45 +0100

    Organise code into methods

--- directory.rb
+++ directory.rb
@@ -13,9 +13,21 @@ students =[
   "Norman Bates"
 ]
 
+#Define methods to print the header, names and footer
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
+end
 
-# Then we print the number of students in the Academy
-puts "The students of Villains Academy"
-puts "----------"
-students.each {|student| puts student}
-puts "Overall we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+def print(names)
+  names.each {|name| puts name}
+end
+
+# Call the methods to run the program
+print_header
+print(students)
+print_footer(students)
